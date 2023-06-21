615dbf7c6424063c1dda817cf6cb40f2f6a28b8b
unknown <matheusdalpra@hotmail.com>
2023-06-20 21:05:38 -0300

    first commit

---
+++ jogo.js
@@ -0,0 +1,120 @@
+			
+var altura = 0
+var largura = 0
+var vidas = 1
+var tempo = 10
+
+var criaMoscaTempo = 3000
+
+
+var nivel = window.location.search
+nivel = nivel.replace('?', '')
+
+if(nivel === 'normal') {
+	criaMoscaTempo = 1500
+} else if(nivel === 'dificil') {
+	criaMoscaTempo = 1000
+} else if (nivel === 'chucknorris') {
+	criaMoscaTempo = 750
+}
+
+
+function ajustaTamanhoPalcoJogo() {
+	 altura = window.innerHeight
+	 largura = window.innerWidth
+
+	 console.log(largura, altura)
+}
+
+ajustaTamanhoPalcoJogo()
+
+var cronometro = setInterval(function() {
+
+	tempo -= 1
+
+	if(tempo < 0) {
+		clearInterval(cronometro)
+		clearInterval(criaMosca)
+		window.location.href = 'vitoria.html'
+	} else {
+	document.getElementById('cronometro').innerHTML = tempo
+
+	}
+}, 1000)
+
+//posição do mosquito
+function posicaoRandomica() {
+
+	//remover mosquito anterior caso exista
+	if(document.getElementById('mosca')) {
+	document.getElementById('mosca').remove()
+
+	//console.log('v' + vidas)
+	if(vidas > 3) {
+
+		window.location.href = 'fim_de_jogo.html'
+	} else{
+	document.getElementById('v' + vidas).src="imagens/coracao_vazio.png"
+
+	vidas++
+}
+}
+
+	var posicaoX = Math.floor(Math.random() * largura) - 90
+	var posicaoY = Math.floor(Math.random() * altura) - 90
+
+	posicaoX = posicaoX < 0 ? 0 : posicaoX
+	posicaoY = posicaoY < 0 ? 0 : posicaoY
+
+	console.log(posicaoX, posicaoY)
+	//fim posição mosquito
+
+	//criar o elemento html
+	var mosca = document.createElement('img')
+	mosca.src = 'imagens/mosca.png'
+	mosca.className = tamanhAleatorio() + ' ' + ladoAleatorio()
+	mosca.style.left = posicaoX  + 'px'
+	mosca.style.top = posicaoY  + 'px'
+	mosca.style.position = 'absolute'
+	mosca.id = 'mosca'
+	mosca.onclick = function() {
+		this.remove()
+	}
+
+	//incluindo no body na página
+	document.body.appendChild(mosca)
+
+	
+
+	
+}
+
+//Tamanho mosquito quando reaparecer
+function tamanhAleatorio() {
+	var classe = Math.floor(Math.random() * 3)
+	
+	switch(classe) {
+
+	case 0:
+		return 'mosca1'
+	case 1:
+		return 'mosca2'
+	case 2:
+		return 'mosca3'
+	}
+}
+
+//Troca de lado do mosquito quando reaparecer
+function ladoAleatorio() {
+
+	var classe = Math.floor(Math.random() * 2)
+	
+	switch(classe) {
+
+	case 0:
+		return 'ladoA'
+	case 1:
+		return 'ladoB'
+	
+	}
+}
